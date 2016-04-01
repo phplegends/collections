918f755b96fae903c701c2b0d18cc7c56bbed0a8
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-04-01 13:33:40 -0300

    Recursive Collection created

---
+++ src/RecursiveCollection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PHPLegends\Collections;
+
+use RecursiveArrayIterator;
+
+class RecursiveCollection extends Collection
+{
+    /**
+     * @{inheritdoc}
+     * */
+    public function setItems(array $items)
+    {   
+        foreach ($items as &$value)
+        {
+            is_array($value) && $value = new static($value);
+        }
+
+        parent::setItems($items);
+
+        return $this;
+    }
+
+    /**
+     * Detects if the index passed is a recursive in collection
+     * @param int|string
+     * */
+    public function isRecursive($key)
+    {
+        return $this->get($key) instanceof self;
+    }
+
+    /**
+     * Overwrites the parent method to make a recursive iterator
+     * @return \RecursiveArrayIterator
+     * */
+    public function getIterator()
+    {
+        return new RecursiveArrayIterator(parent::getIterator());
+    }
+}
