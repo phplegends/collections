d63ab083021fb8fcc8d0df3e48e677530d37e175
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-04-01 14:32:37 -0300

    Removing "tabs" for Accessible

--- src/Contracts/Accessible.php
+++ src/Contracts/Accessible.php
@@ -7,82 +7,82 @@ namespace PHPLegends\Collections\Contracts;
  * 
  * */
 interface Accessible
-{	
-	/**
-	 * Description
-	 * @param int|key $key 
-	 * @param mixed $value 
-	 * @return self
-	 */
-	public function set($key, $value);
+{   
+    /**
+     * Description
+     * @param int|key $key 
+     * @param mixed $value 
+     * @return self
+     */
+    public function set($key, $value);
 
-	/**
-	 * Description
-	 * @param int|key $key 
-	 * @return mixed
-	 */
-	public function get($key);
+    /**
+     * Description
+     * @param int|key $key 
+     * @return mixed
+     */
+    public function get($key);
 
-	/**
-	 * Checks if key exists in Collection
-	 * @param int|string $key 
-	 * @return boolean
-	 */
-	public function has($key);
+    /**
+     * Checks if key exists in Collection
+     * @param int|string $key 
+     * @return boolean
+     */
+    public function has($key);
 
-	/**
-	 * Delete an item from Collection by index
-	 * @return mixed
-	 * */
-	public function delete($key);
+    /**
+     * Delete an item from Collection by index
+     * @return mixed
+     * */
+    public function delete($key);
 
-	/**
-	 * All keys of Collection 
-	 * @return array
-	 * */
-	public function keys();
+    /**
+     * All keys of Collection 
+     * @return array
+     * */
+    public function keys();
 
-	/**
-	 * @param array $items
-	 * @param boolean $recursive
-	 * @return $this
-	 * */
-	public function merge(array $items, $recursive = false);
+    /**
+     * @param array $items
+     * @param boolean $recursive
+     * @return $this
+     * */
+    public function merge(array $items, $recursive = false);
 
-	/**
-	 * Replaces items in Collection
-	 * @param array $items 
-	 * @param boolean $recursive 
-	 * @return $this
-	 */
-	public function replace(array $items, $recursive = false);
+    /**
+     * Replaces items in Collection
+     * @param array $items 
+     * @param boolean $recursive 
+     * @return $this
+     */
+    public function replace(array $items, $recursive = false);
 
-	/**
-	 * Returns elements in collection specified in $keys
-	 * @param array $keys
-	 * @return Collection
-	 * */
-	public function only(array $keys);
+    /**
+     * Returns elements in collection specified in $keys
+     * @param array $keys
+     * @return Collection
+     * */
+    public function only(array $keys);
 
-	/**
-	 * Returns all elements of Collection, except the specified in $keys
-	 * @param array $keys
-	 * @return Collection
-	 * */
-	public function except(array $keys);
+    /**
+     * Returns all elements of Collection, except the specified in $keys
+     * @param array $keys
+     * @return Collection
+     * */
+    public function except(array $keys);
 
-	/**
-	 * Searches element by index in collecion
-	 * @param int|string $key
-	 * @return int|string|false
-	 * */
-	public function search($key);
+    /**
+     * Searches element by index in collecion
+     * @param int|string $key
+     * @return int|string|false
+     * */
+    public function search($key);
 
-	/**
-	 * Get an item from collection and, if doesnt have, returns default value
-	 * @param int|string $key
-	 * @param mixed|null $default
-	 * @return mixed
-	 * */
-	public function getOrDefault($key, $default = null);
+    /**
+     * Get an item from collection and, if doesnt have, returns default value
+     * @param int|string $key
+     * @param mixed|null $default
+     * @return mixed
+     * */
+    public function getOrDefault($key, $default = null);
 }
