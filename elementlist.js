327e92c0c805347adda903860cd0fe77f18f28d2
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-04-05 13:40:18 -0300

    Initial commit

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","PHPLegends\\Collections\\Collection"],["c","PHPLegends\\Collections\\Contracts\\Accessible"],["c","PHPLegends\\Collections\\Contracts\\Arrayable"],["c","PHPLegends\\Collections\\Contracts\\Collectible"],["c","PHPLegends\\Collections\\Contracts\\Modifiable"],["c","PHPLegends\\Collections\\Contracts\\Validatable"],["c","PHPLegends\\Collections\\Exceptions\\CollectionException"],["c","PHPLegends\\Collections\\ListCollection"],["c","PHPLegends\\Collections\\MathCollection"],["c","PHPLegends\\Collections\\RecursiveCollection"]];
